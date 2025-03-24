521cfa92a3c1752423ad40b3613e6a55edaf5413
ADEGBOLA ABIOLA MANSOUROU <abiole68@gmail.com>
2025-03-24 16:09:29 +0000

    modification 16:09 24 03 2025

--- src/smartStock/Composants/Recupdf.jsx
+++ src/smartStock/Composants/Recupdf.jsx
@@ -15,37 +15,35 @@ const DevisPDF = ({ data, facturesInfo }) => {
           <View style={styles.headerText}>
             <Text style={styles.title}>FACTURE</Text>
             <Text>Date: {data.createdAt ? data.createdAt.toLocaleDateString() : "Date inconnue"}</Text>
-            {/* <Text>Numéro: 001/04</Text> */}
-            {/* <Text>Valable: {data?.valDate || ""}</Text> */}
           </View>
         </View>
 
         {/* Infos Entreprise */}
         <View style={{marginLeft:30}}>
         <Image src={logo} style={styles.logo} />
-          <Text style={styles.bold}>{facturesInfo?.nomEntreprise || ""}</Text>
-          <Text>{facturesInfo?.statutJuridique || ""}</Text>
-          <Text>{facturesInfo?.adresse || ""}</Text>
-          <Text>{facturesInfo?.phone || ""}</Text>
-          <Text>{facturesInfo?.email || ""}</Text>
+          <Text style={{fontWeight: 700, marginTop:5, lineHeight: 1.5, fontSize: 12}}>{facturesInfo?.nomEntreprise || ""}</Text>
+          <Text style={styles.texte}>{facturesInfo?.statutJuridique || ""}</Text>
+          <Text style={styles.texte}>{facturesInfo?.adresse || ""}</Text>
+          <Text style={styles.texte}>{facturesInfo?.phone || ""}</Text>
+          <Text style={styles.texte}>{facturesInfo?.email || ""}</Text>
         </View>
 
         {/* Infos Client */}
         <View style={[{marginTop:-30, marginRight:80}, styles.clientInfo]}>
           {/* <Text style={styles.bold}>Le client</Text> */}
-          <Text>{data?.nomClient || ""}</Text>
-          <Text>{data?.adresseClient || ""}</Text>
-          <Text>{data?.phone || ""}</Text>
-          <Text>{data?.destination || ""}</Text>
+          <Text style={{fontWeight: 700, marginTop:5, lineHeight: 1.5, fontSize: 12}}>{data?.nomClient || ""}</Text>
+          <Text style={styles.texte}>{data?.adresseClient || ""}</Text>
+          <Text style={styles.texte}>{data?.phone || ""}</Text>
+          <Text style={styles.texte}>{data?.destination || ""}</Text>
         </View>
 
         {/* Tableau des articles */}
         <View style={styles.table}>
           <View style={styles.rowHeader}>
-            <Text style={styles.cell}>Désignation</Text>
-            <Text style={styles.cell}>Qté</Text>
-            <Text style={styles.cell}>Prix HT</Text>
-            <Text style={styles.cell}>TOTAL HT</Text>
+            <Text style={{padding: 8, flex:1, textAlign: "left"}}>Désignation</Text>
+            <Text style={{padding: 8, flex:1, textAlign: "left", borderLeftWidth: 0.2, borderRightWidth: 0.2, borderStyle: 'solid',  borderColor: ' rgba(0, 0, 0, 0.1)'}}>Qté</Text>
+            <Text style={{padding: 8, flex:1, textAlign: "left"}}>Prix HT (F CFA)</Text>
+            <Text style={{padding: 8, flex:1, textAlign: "left", borderLeftWidth: 0.2, borderStyle: 'solid',  borderColor: ' rgba(0, 0, 0, 0.1)'}}>TOTAL HT (F CFA)</Text>
           </View>
 
           {data?.materiels &&
@@ -53,17 +51,24 @@ const DevisPDF = ({ data, facturesInfo }) => {
               <View style={styles.row} key={k}>
                 <Text style={styles.cell}>{el.titre}</Text>
                 <Text style={styles.cell}>{el.quantite}</Text>
-                <Text style={styles.cell}>{el.prixUnitaire} F CFA</Text>
-                <Text style={styles.cell}>{parseInt(el.quantite) * parseInt(el.prixUnitaire)} F CFA</Text>
+                <Text style={styles.cell}>{el.prixUnitaire}</Text>
+                <Text style={styles.cell}>{parseInt(el.quantite) * parseInt(el.prixUnitaire)}</Text>
               </View>
             ))}
         </View>
 
         {/* Totaux */}
-        <View style={styles.totals}>
-          <Text>Total HT : {data?.total} F</Text>
-          <Text>Remise : {data?.remise} %</Text>
-          <Text style={styles.bold}>TOTAL HT avec remise : {data?.montant} F CFA</Text>
+        <View style={{marginTop: 30, flexDirection: 'row', justifyContent: 'flex-start'}}>
+        <View style={{fontSize: 12}}>
+          <Text style={[styles.cell, {width: 300}]}>Total HT</Text>
+          <Text style={[styles.cell, {width: 300}]}>Remise</Text>
+          <Text style={[styles.cell, {width: 300}]}>TOTAL HT avec remise</Text>
+        </View>
+        <View style={{fontSize: 12, marginLeft: 50}}>
+          <Text style={{padding: 10}}>{data?.total} F</Text>
+          <Text style={{padding: 10}}>{data?.remise} %</Text>
+          <Text style={{padding: 10}}>{data?.montant} F CFA</Text>
+        </View>
         </View>
 
         {/* Conditions de paiement */}
@@ -120,7 +125,7 @@ const Recupdf = ({ data, facturesInfo }) => {
 // 🔹 Styles adaptés pour @react-pdf/renderer
 const styles = StyleSheet.create({
   page: {
-    fontFamily: 'Times-Roman',
+    // fontFamily: 'system-ui',
     fontSize: 12,
     padding: 30,
     backgroundColor: '#fff',
@@ -129,22 +134,19 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: 'row',
     justifyContent: "flex-end",
-    // borderBottomWidth: 1,
-    // borderBottomColor: '#ccc',
-    // paddingBottom: 10,
     marginBottom: 5,
   },
   logo: {
     width: 100,
-    height: 60,
+    height: 80,
   },
   headerText: {
     textAlign: 'right',
   },
   title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#4a90e2',
+    fontSize: 35,
+    fontWeight: 800,
+    color: "rgb(74, 144, 226)",
   },
   section: {
     marginBottom: 10,
@@ -156,34 +158,45 @@ const styles = StyleSheet.create({
   bold: {
     fontWeight: 'bold',
   },
+  texte: {
+    fontSize: 10,
+    lineHeight: 1.5,
+  },
   table: {
     marginTop: 30,
-    borderWidth: 1,
-    borderColor: '#ddd',
+    // borderWidth: 1,
+    // borderColor: '#ddd',
   },
   rowHeader: {
     flexDirection: 'row',
-    backgroundColor: '#4a90e2',
+    backgroundColor: 'rgb(74, 144, 226)',
     color: '#fff',
     paddingVertical: 5,
+    borderWidth: 0.2,
+    borderStyle: 'solid',
+    borderColor: ' rgb(74, 144, 226)',
   },
   row: {
     flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
-    paddingVertical: 4,
+    // borderWidth: 0.2,
+    // bordertyle: 'solid',
+    // borderColor: ' rgba(0, 0, 0, 0.1)',
+    // paddingVertical: 4,
   },
   cell: {
     flex: 1,
     textAlign: 'left',
-    padding: 5,
+    padding: 10,
     fontSize: 10,
+    borderWidth: 0.2,
+    borderStyle: 'solid',
+    borderColor: ' rgba(0, 0, 0, 0.1)',
   },
   totals: {
     marginTop: 10,
     padding: 10,
-    borderTopWidth: 1,
-    borderTopColor: '#000',
+    // borderTopWidth: 1,
+    // borderTopColor: '#000',
     textAlign: 'right',
   },
   paymentTerms: {
